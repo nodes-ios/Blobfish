9038d60d14a9014b31b7e15c34a4068f787f40c6
Kasper Welner <kawe@nodes.dk>
2016-03-13 11:53:38 +0100

    Initial Commit

---
+++ Blobfish/Blobfish.h
@@ -0,0 +1,19 @@
+//
+//  Blobfish.h
+//  Blobfish
+//
+//  Created by Kasper Welner on 13/03/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Blobfish.
+FOUNDATION_EXPORT double BlobfishVersionNumber;
+
+//! Project version string for Blobfish.
+FOUNDATION_EXPORT const unsigned char BlobfishVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blobfish/PublicHeader.h>
+
+
