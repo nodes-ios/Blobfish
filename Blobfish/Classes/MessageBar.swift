562e77458a6620be7a0f7f2098123fdd2733e130
Nicolai <nicolai@famharbo.dk>
2019-02-18 11:42:43 +0100

    minor label changes

--- Blobfish/Classes/MessageBar.swift
+++ Blobfish/Classes/MessageBar.swift
@@ -17,7 +17,7 @@ public class MessageBar: UIWindow {
         self.translatesAutoresizingMaskIntoConstraints = false
         label.translatesAutoresizingMaskIntoConstraints = false
         label.frame.origin.y = LayoutUtils.safeAreaTop()
-        label.frame.size.height = 15.0
+        label.frame.size.height = 16.0
         self.addSubview(label)
         
         self.label.backgroundColor = UIColor.clear
@@ -28,9 +28,10 @@ public class MessageBar: UIWindow {
         }
         self.label.lineBreakMode = NSLineBreakMode.byTruncatingTail
         self.label.numberOfLines = 1
-        self.label.minimumScaleFactor = 0.2
+//        self.label.minimumScaleFactor = 0.2
         self.label.textColor = UIColor.white
-        self.label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        self.label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.sizeToFit()
         self.backgroundColor = UIColor.red
         self.isHidden = true
         self.windowLevel = UIWindowLevelStatusBar+1;
@@ -39,7 +40,7 @@ public class MessageBar: UIWindow {
     public override func layoutSubviews() {
         label.frame = self.bounds.insetBy(dx: 8, dy: 0)
         label.frame.origin.y = LayoutUtils.safeAreaTop()
-        label.frame.size.height = 15.0
+        label.frame.size.height = 16.0
     }
     
     required public init?(coder aDecoder: NSCoder) {
